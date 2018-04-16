bdacd50725ea0e8aa26fd8a1ddaa70e96b8da2b5
Sudharaka Palamakumbura <sudharakap@mun.ca>
2018-04-15 18:19:19 -0700

    Prepare cleanup of useless master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { MainHeaderComponent } from './main-header/main-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainHeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
