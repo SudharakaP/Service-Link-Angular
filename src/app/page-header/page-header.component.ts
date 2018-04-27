60fbaa74554a5f147ff1e087f0571d13ab7fc994
Sudharaka <sudharaka@service-link.us>
2018-04-26 22:43:10 -0700

    Finalize generic page header

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -1,21 +1,13 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
-  templateUrl: '../main-header/main-header.component.html',
+  templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.css']
 })
 export class PageHeaderComponent implements OnInit {
   pageScrolled: boolean;
   constructor() { }
   ngOnInit() {
-  }
-
-  @HostListener("window:scroll", []) 
-  addShrinkClass () { 
-    if (window.pageYOffset > 100)
-      this.pageScrolled = true;
-    else
-      this.pageScrolled = false;
   }
 }
