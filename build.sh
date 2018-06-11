0309032f7f47c77d62af0f8974322b6a9f8cb6fd
Sudharaka <sudharaka@service-link.us>
2018-06-10 21:05:06 -0700

    New deploy key

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="development"
-ENCRYPTION_LABEL="2fbc6d173e85"
+ENCRYPTION_LABEL="5cc90922a0ab"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
